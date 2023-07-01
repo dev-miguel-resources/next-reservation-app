63b5ad5578629da3a30f110f439c02ea2cd289b1
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-07-01 11:08:34 -0400

    first steps in custom hooks, state managament, avatar, menuItem and userMenu components

---
+++ src/custom-hooks/interfaces/registerModal.interface.ts
@@ -0,0 +1,5 @@
+export default interface RegisterModalStore {
+	isOpen: boolean;
+	onOpen: () => void;
+	onClose: () => void;
+}
---
+++ src/molecules/avatar/Avatar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import Image from "next/image";
+
+const Avatar = () => (
+  <Image className="rounded-full" height="30" width="30" alt="Avatar" src="/images/placeholder.jpg" />
+);
+
+export default Avatar;
