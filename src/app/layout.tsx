1f0374354d2325847400c93492674061de8b1564
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-08-01 21:22:05 -0400

    clientProcessor, Toast Provider and configuration and define of entities project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,29 @@
 import { Open_Sans } from "next/font/google";
+import ClientProcessor from "@molecules/clientProcessor/ClientProcessor";
 import Navbar from "@molecules/navbar/Navbar";
 import RegisterModal from "@molecules/modals/RegisterModal";
+import ToasterProvider from "@providers/ToasterProvider";
 import "@atoms/globals.css";
 
 export const metadata = {
-	title: "Alquiler de alojamientos vacacionales y apartamentos",
-	description:
-		"Encuentra todo tipo de alojamientos personalizados desde pequeños apartamentos y casas a grandes hoteles y resorts.",
+  title: "Alquiler de alojamientos vacacionales y apartamentos",
+  description:
+    "Encuentra todo tipo de alojamientos personalizados desde pequeños apartamentos y casas a grandes hoteles y resorts.",
 };
 
 const font = Open_Sans({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en">
-			<body className={font.className}>
-				<Navbar />
-				<RegisterModal />
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientProcessor>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+			 {children}
+        </ClientProcessor>
+      </body>
+    </html>
+  );
 }
